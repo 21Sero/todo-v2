936791ad3c6dc8c78d49a8aac45748eb8e4796e3
Sero <122702052+21Sero@users.noreply.github.com>
2023-05-16 16:33:29 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.scss'
 import TodoForm from './components/TodoForm/TodoForm'
 import TodoList from './components/TodoList/TodoList'
 import { initiacValue, reducer } from './hook/reducer'
+import GithubBtn from "./components/githubBtn/GithubBtn.jsx";
+
 
 function App() {
 	const [state, dispatch] = useReducer(reducer, initiacValue)
 
 	return (
 		<main className='main'>
+			<GithubBtn />
 			<TodoForm dispatch={dispatch} />
 			<TodoList Todos={state} dispatch={dispatch} />
 		</main>
