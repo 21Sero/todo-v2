98ee08c427d684f8cf61a28d7e0291be204b1283
Sero <122702052+21Sero@users.noreply.github.com>
2023-05-16 16:35:16 +0400

    Update TodoForm.jsx

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -18,7 +18,7 @@ export default function TodoForm({ dispatch }) {
 			<form className='container__form' onSubmit={onSubmitForm}>
 				<label className='container__form__input__label' htmlFor='input'>
 					<FcTodoList className='container__form__input__icon' />
-					<input type="text" className='container__form__input' id="input" placeholder='Add your todo...' />
+					<input type="text" className='container__form__input' required id="input" placeholder='Add your todo...' />
 				</label>
 				<button type="submit" className='container__form__button'>
 					<MdAddTask className='container__form__button__icon' />
